7166f4fa233b48598dc41e9b5ebec4173c41733d
timfoil <timpks@me.com>
2015-12-04 09:08:26 -0500

    Removed debug code from mainActivity
    Signed-off-by: timfoil <timpks@me.com>

--- app/src/main/java/com/tapjar/tapjar/MainActivity.java
+++ app/src/main/java/com/tapjar/tapjar/MainActivity.java
@@ -23,11 +23,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        message = "";
-        Intent intent = new Intent(MainActivity.this, PaymentActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 
 
